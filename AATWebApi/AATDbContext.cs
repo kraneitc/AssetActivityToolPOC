9db5e0cfb1d1a49297d1c8741df51bf6455e67ed
Steve Lunnay <steve.lunnay@sapowernetworks.com.au>
2020-07-01 10:20:31 +0930

    add api

---
+++ AATWebApi/AATDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AATWebApi;
+using Microsoft.EntityFrameworkCore;
+
+namespace AATWebApp
+{
+    public class AATDbContext : DbContext
+    {
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:pocattdb.database.windows.net,1433;Initial Catalog=pocattdb;Persist Security Info=False;User ID=pocattdb;Password=I*ecQF@aK28uH*W^5*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
---
+++ AATWebApi/Controllers/AATTestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AATWebApp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AATWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AATTestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<AATTestController> _logger;
+
+        public AATTestController(ILogger<AATTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            using var ctx = new AATDbContext();
+            return ctx.Product.
+        }
+    }
+}
