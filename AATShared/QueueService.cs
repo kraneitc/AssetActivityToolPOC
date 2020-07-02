851effee89e97b185cbf14f64cbb3f7a6c5c27cf
Steve Lunnay <steve.lunnay@sapowernetworks.com.au>
2020-07-02 12:16:42 +0930

    works

---
+++ AATShared/QueueService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AATShared
+{
+    class QueueService
+    {
+
+        private static async Task<Response<SendReceipt>> QueueMessageAsync(string accountName, string queueName, string message)
+        {
+            var queueEndpoint = $"https://{accountName}.queue.core.windows.net/{queueName}";
+            var queueClient = new QueueClient(new Uri(queueEndpoint), new DefaultAzureCredential());
+
+            await queueClient.CreateIfNotExistsAsync();
+            return await queueClient.SendMessageAsync(message);
+        }
+    }
+}
--- AATWebApi/Controllers/QueueController.cs
+++ AATWebApi/Controllers/QueueController.cs
@@ -11,10 +11,6 @@ using System.Threading.Tasks;
 using Azure.Identity;
 using Azure.Storage.Queues.Models;
 using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.WindowsAzure.Storage.Queue;
-using Microsoft.Azure.Storage.Auth;
-using Microsoft.Rest;
-using Microsoft.Azure.Storage;
 using TokenCredential = Azure.Core.TokenCredential;
 
 namespace AATWebApi.Controllers
@@ -33,25 +29,17 @@ namespace AATWebApi.Controllers
         [HttpGet]
         public async Task<string> Get()
         {
-            await QueueMessageAsync("pocaatsa", "aatqueue", "message");
-
-            return $"Message inserted";
+            var result = await QueueMessageAsync("pocaatsa", "aatqueue", "Message from AAT API");
+            return $"Message inserted at {result.Value.InsertionTime}";
         }
 
-        private static async Task QueueMessageAsync(string accountName, string queueName, string message)
+        private static async Task<Response<SendReceipt>> QueueMessageAsync(string accountName, string queueName, string message)
         {
-            var token = new AzureServiceTokenProvider().GetAccessTokenAsync($"https://storage.azure.com/").Result;
-            var creds = new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(new Microsoft.WindowsAzure.Storage.Auth.TokenCredential(token));
+            var queueEndpoint = $"https://{accountName}.queue.core.windows.net/{queueName}";
+            var queueClient = new QueueClient(new Uri(queueEndpoint), new DefaultAzureCredential());
 
-            var queueEndpoint = $"https://{accountName}.queue.core.windows.net/";
-            //var queueClient = new QueueClient(new Uri(queueEndpoint), new DefaultAzureCredential(true));
-            var queueClient = new CloudQueueClient(new Uri(queueEndpoint), creds);
-
-            var queue = queueClient.GetQueueReference(queueName);
-
-
-            await queue.CreateIfNotExistsAsync();
-            await queue.AddMessageAsync(new CloudQueueMessage(message));
+            await queueClient.CreateIfNotExistsAsync();
+            return await queueClient.SendMessageAsync(message);
         }
     }
 }
